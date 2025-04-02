d97daaaeade5b273eba535e48ebd433c3d09949f
Kaushik Shankar <kaushik.s23@gmail.com>
2025-04-02 20:04:55 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
-
-from fastapi import FastAPI
 import os
+from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return {"message": "ZenBot test app is live!"}
-
-@app.get("/secret")
-async def get_secret():
-    value = os.getenv("TEST_SECRET", "Not Set")
-    return {"TEST_SECRET": value}
-
-
-# In[5]:
-
-
-
-
+def read_root():
+    api_key = os.environ.get("MY_SECRET_KEY")
+    return {"message": "Secret is working!", "your_key": api_key or "No key found"}
